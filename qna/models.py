6b08e284eac7f684d0fc9732507d9f68debd3c0e
CHANN <chann@chann.kr>
2016-10-23 16:44:55 +0900

    Refactoring: Fix indentation.

--- qna/models.py
+++ qna/models.py
@@ -22,7 +22,7 @@ class Answer(models.Model):
     question = models.ForeignKey('qna.Question', related_name='answers')
     user = models.ForeignKey('auth.User')
     content = models.TextField()
-	### avoid reinventing the wheel, consider django packages someone maked it.
+    ### avoid reinventing the wheel, consider django packages someone maked it.
     # tag=models.ManyToManyField(Tag)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(default=timezone.now, blank=True, null=True)
